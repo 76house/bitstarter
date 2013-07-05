5694e9a009363bf47ac6daaec44b79fb4600b050
76house <martin.marek@carabana.cz>
2013-07-05 20:14:26 +0000

    Read msg from file

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
-var express=require('express');
+var fs = require('fs');
+var express = require('express');
 var app = express();
+var text = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send("Hello world!");
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
   console.log("Listening on " + port);
 });
+
