c9304e65c6108cb252e3e48981b8a0f00eee7747
76house <martin.marek@carabana.cz>
2013-07-06 00:24:07 +0000

    URL support added

--- grader.js
+++ grader.js
@@ -24,36 +24,67 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var url = require('url');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "http://warm-bayou-3554.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if (!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
+var assertUrlIsValid = function(inUrl) {
+    if (url.parse(inUrl) == null)
+    {
+	console.log("%s is not a valid URL. Exiting.", inUrl);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code                                
+    }
+    return inUrl;
+}
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+var doChecks = function($, checksfile) {
+    var checks = JSON.parse(fs.readFileSync(checksfile)).sort();
+    var out = {};
+    for(var ii in checks) {                                                                                                                                                  
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;                                                                                                                                           
+    }
+
+    var outJson = JSON.stringify(out, null, 4);                                                                                                                        
+    console.log(outJson);
 };
+
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+    doChecks($, checksfile);
 };
+
+var buildfn = function(checksfile) {
+    return function(result, response) {
+	if (result instanceof Error) {
+	    console.log('Error: ' + util.format(response.message));
+	} else {
+	    $ = cheerio.load(result);
+	    doChecks($, checksfile);
+	}
+    };
+}
+
+var checkHtmlUrl = function(htmlurl, checksfile) {
+    var checkfn = buildfn(checksfile);
+    rest.get(htmlurl).on('complete', checkfn);
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -64,11 +95,14 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url_address>', 'URL to index.html', clone(assertUrlIsValid))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var checkJson = (program.url != null) ? 
+        checkHtmlUrl(program.url, program.checks) :
+	checkHtmlFile(program.file, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlUrl = checkHtmlUrl;
 }
