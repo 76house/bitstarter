dd4442b15d7b8ee79fea77a9114c436378fc3bec
76house <martin.marek@carabana.cz>
2013-07-21 18:22:00 +0000

    Bitstarter website scaffolding

--- web.js
+++ web.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var url = require('url');
 var express = require('express');
 var app = express();
 var text = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
+
+  var pathname = url.parse(request.url).pathname;
+  console.log("Request for " + pathname + " received.");
+  if (pathname == '/') pathname = 'index.html';
+  var text = fs.readFileSync(pathname).toString();
   response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+app.configure(function(){
+  app.use(express.static(__dirname));
+});
+
+var port = process.env.PORT || 8080;
+
 app.listen(port, function(){
   console.log("Listening on " + port);
 });
